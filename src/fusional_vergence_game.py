c70eb053d59145d102bd062bd7ad8aa9adf81629
Michele Rizzo <mik3.rizzo@gmail.com>
2025-05-22 05:12:07 +0200

    Refactoring: removed _update_dynamic_positions() and some class attributes (mouse_rel cursor_red/blue positions) to simplify the code. Modified control to check if the cursor is on the square.

--- src/fusional_vergence_game.py
+++ src/fusional_vergence_game.py
@@ -25,7 +25,6 @@ class FusionalVergenceGame:
 
         self.font = pygame.font.SysFont(None, 36)
         self.font_small = pygame.font.SysFont(None, 28)
-
         self.clock = pygame.time.Clock()
         
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -37,30 +36,24 @@ class FusionalVergenceGame:
         self.offset = self.cfg.initial_offset
         self.step = self.cfg.step
 
-        self.layer_x = (self.screen_width - self.cfg.layer_width) // 2
+        self.layer_x = (self.screen_width - self.cfg.layer_width) // 2  # Relative to the screen
         self.layer_y = (self.screen_height - self.cfg.layer_height) // 2
         
-        self.square_x = random.randint(0, self.cfg.layer_width - self.cfg.square_size)
-        self.square_y = random.randint(0, self.cfg.layer_height - self.cfg.square_size)
+        self.square_rel_x = random.randint(0, self.cfg.layer_width - self.cfg.square_size)  # Relative to the layer
+        self.square_rel_y = random.randint(0, self.cfg.layer_height - self.cfg.square_size)
     
         self.noise_matrix = self._create_noise_matrix()
-        self.red_layer = self._create_layer(RED_LAYER_COLOR, self.square_x, self.square_y)
-        self.blue_layer = self._create_layer(BLUE_LAYER_COLOR, self.square_x, self.square_y)
+        self.red_layer_surface = self._create_layer_surface(RED_LAYER_COLOR)
+        self.blue_layer_surface = self._create_layer_surface(BLUE_LAYER_COLOR)
+        self.cursor_surface = pygame.Surface((CURSOR_RADIUS * 2, CURSOR_RADIUS * 2), pygame.SRCALPHA)
 
-        self.mouse_rel_x = 0
-        self.mouse_rel_y = 0
-        self.cursor_red_x = 0
-        self.cursor_red_y = 0
-        self.cursor_blue_x = 0
-        self.cursor_blue_y = 0
-
-        self.last_correct = None
+        self.last_result = None
 
     def _create_noise_matrix(self):
         return [[random.randint(0, self.cfg.noise_intensity) for _ in range(self.cfg.layer_width)] for _ in range(self.cfg.layer_height)]
 
-    def _create_layer(self, color, square_x, square_y):
-        layer = pygame.Surface((self.cfg.layer_width, self.cfg.layer_height))
+    def _create_layer_surface(self, color):
+        layer_surface = pygame.Surface((self.cfg.layer_width, self.cfg.layer_height))
 
         for y in range(self.cfg.layer_height):
             for x in range(self.cfg.layer_width):
@@ -68,13 +61,13 @@ class FusionalVergenceGame:
 
                 if color == RED_LAYER_COLOR:
                     xy_is_inside_square = (
-                        square_x <= x < square_x + self.cfg.square_size and
-                        square_y <= y < square_y + self.cfg.square_size
+                        self.square_rel_x <= x < self.square_rel_x + self.cfg.square_size and
+                        self.square_rel_y <= y < self.square_rel_y + self.cfg.square_size
                     )
                 elif color == BLUE_LAYER_COLOR:
                     xy_is_inside_square = (
-                        square_x + self.disparity <= x < square_x + self.cfg.square_size + self.disparity and
-                        square_y <= y < square_y + self.cfg.square_size
+                        self.square_rel_x + self.disparity <= x < self.square_rel_x + self.cfg.square_size + self.disparity and
+                        self.square_rel_y <= y < self.square_rel_y + self.cfg.square_size
                     )
 
                 if xy_is_inside_square and color == BLUE_LAYER_COLOR:
@@ -84,10 +77,10 @@ class FusionalVergenceGame:
                         val = self.noise_matrix[y][src_x]
                 
                 if color == RED_LAYER_COLOR:
-                    layer.set_at((x, y), (val, 0, 0))
+                    layer_surface.set_at((x, y), (val, 0, 0))
                 elif color == BLUE_LAYER_COLOR:
-                    layer.set_at((x, y), (0, 0, val))
-        return layer
+                    layer_surface.set_at((x, y), (0, 0, val))
+        return layer_surface
 
     def _handle_input(self):
         for event in pygame.event.get():
@@ -97,71 +90,59 @@ class FusionalVergenceGame:
                 if event.key == pygame.K_ESCAPE:
                     return False  # Signal to stop running
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self._process_mouse_click()
+                self._on_mouse_click()
         return True # Signal to continue running
 
-    def _process_mouse_click(self):
-        # Convert mouse relative coordinates to red layer's local coordinates
-        # self.mouse_rel_x and self.mouse_rel_y are updated in _update_dynamic_positions
-        cursor_on_red_layer_x = self.cfg.layer_width // 2 + self.mouse_rel_x
-        cursor_on_red_layer_y = self.cfg.layer_height // 2 + self.mouse_rel_y
+    def _on_mouse_click(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
+        mouse_rel_x = mouse_x - self.layer_x
+        mouse_rel_y = mouse_y - self.layer_y 
+
+        # Note: the calculation are made using the virtual centered layer and cursor
         click_is_on_square = (
-            0 <= cursor_on_red_layer_x < self.cfg.layer_width and
-            0 <= cursor_on_red_layer_y < self.cfg.layer_height and
-            self.square_x <= cursor_on_red_layer_x < self.square_x + self.cfg.square_size and
-            self.square_y <= cursor_on_red_layer_y < self.square_y + self.cfg.square_size
+            0 <= mouse_rel_x <= self.cfg.layer_width and
+            0 <= mouse_rel_y <= self.cfg.layer_height and
+            self.square_rel_x <= mouse_rel_x <= self.square_rel_x + self.cfg.square_size and
+            self.square_rel_y <= mouse_rel_y <= self.square_rel_y + self.cfg.square_size
         )
 
         if click_is_on_square:
             if self.offset < self.max_offset:
                 self.offset += self.step
-            self.last_correct = True
+            self.last_result = True
         else:
             if self.offset > self.min_offset:
                 self.offset -= self.step
-            self.last_correct = False
+            self.last_result = False
 
-        self.square_x = random.randint(0, self.cfg.layer_width - self.cfg.square_size)
-        self.square_y = random.randint(0, self.cfg.layer_height - self.cfg.square_size)
+        self.square_rel_x = random.randint(0, self.cfg.layer_width - self.cfg.square_size)
+        self.square_rel_y = random.randint(0, self.cfg.layer_height - self.cfg.square_size)
 
-        self.red_layer = self._create_layer(RED_LAYER_COLOR, self.square_x, self.square_y)
-        self.blue_layer = self._create_layer(BLUE_LAYER_COLOR, self.square_x, self.square_y)
-
-    def _update_dynamic_positions(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        
-        self.mouse_rel_x = mouse_x - (self.layer_x + self.cfg.layer_width // 2)
-        self.mouse_rel_y = mouse_y - (self.layer_y + self.cfg.layer_height // 2)
-
-        self.cursor_red_x = self.layer_x - self.offset + self.cfg.layer_width // 2 + self.mouse_rel_x
-        self.cursor_red_y = self.layer_y + self.cfg.layer_height // 2 + self.mouse_rel_y
-        
-        self.cursor_blue_x = self.layer_x + self.offset + self.cfg.layer_width // 2 + self.mouse_rel_x
-        self.cursor_blue_y = self.layer_y + self.cfg.layer_height // 2 + self.mouse_rel_y
+        self.red_layer_surface = self._create_layer_surface(RED_LAYER_COLOR)
+        self.blue_layer_surface = self._create_layer_surface(BLUE_LAYER_COLOR)
 
     def _draw_scene(self):
         self.screen.fill(BACKGROUND_COLOR)
 
         # Draw red and blue layers
-        self.screen.blit(self.red_layer, (self.layer_x - self.offset, self.layer_y), special_flags=pygame.BLEND_ADD)
-        self.screen.blit(self.blue_layer, (self.layer_x + self.offset, self.layer_y), special_flags=pygame.BLEND_ADD)
+        self.screen.blit(self.red_layer_surface, (self.layer_x - self.offset, self.layer_y), special_flags=pygame.BLEND_ADD)
+        self.screen.blit(self.blue_layer_surface, (self.layer_x + self.offset, self.layer_y), special_flags=pygame.BLEND_ADD)
 
         # Draw red and blue cursors
-        cursor_surface = pygame.Surface((CURSOR_RADIUS * 2, CURSOR_RADIUS * 2), pygame.SRCALPHA)
-        pygame.draw.circle(cursor_surface, CURSOR_RED_COLOR, (CURSOR_RADIUS, CURSOR_RADIUS), CURSOR_RADIUS)
-        self.screen.blit(cursor_surface, (self.cursor_red_x - CURSOR_RADIUS, self.cursor_red_y - CURSOR_RADIUS), special_flags=pygame.BLEND_ADD)
-        pygame.draw.circle(cursor_surface, CURSOR_BLUE_COLOR, (CURSOR_RADIUS, CURSOR_RADIUS), CURSOR_RADIUS)
-        self.screen.blit(cursor_surface, (self.cursor_blue_x - CURSOR_RADIUS, self.cursor_blue_y - CURSOR_RADIUS), special_flags=pygame.BLEND_ADD)
-        
-        
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        pygame.draw.circle(self.cursor_surface, CURSOR_RED_COLOR, (CURSOR_RADIUS, CURSOR_RADIUS), CURSOR_RADIUS)
+        self.screen.blit(self.cursor_surface, (mouse_x - self.offset - CURSOR_RADIUS, mouse_y - CURSOR_RADIUS), special_flags=pygame.BLEND_ADD)
+        pygame.draw.circle(self.cursor_surface, CURSOR_BLUE_COLOR, (CURSOR_RADIUS, CURSOR_RADIUS), CURSOR_RADIUS)
+        self.screen.blit(self.cursor_surface, (mouse_x + self.offset - CURSOR_RADIUS, mouse_y - CURSOR_RADIUS), special_flags=pygame.BLEND_ADD)
+
         # Draw the score
         score_text_surface = self.font.render(Strings.MSG_SCORE.format(self.offset), True, TEXT_COLOR)
         self.screen.blit(score_text_surface, (20, 20))
 
-        if self.last_correct is not None:
-            result_color = TEXT_CORRECT_COLOR if self.last_correct else TEXT_WRONG_COLOR
-            result_msg = Strings.MSG_CORRECT if self.last_correct else Strings.MSG_WRONG
+        if self.last_result is not None:
+            result_color = TEXT_CORRECT_COLOR if self.last_result else TEXT_WRONG_COLOR
+            result_msg = Strings.MSG_CORRECT if self.last_result else Strings.MSG_WRONG
             result_text_surface = self.font.render(result_msg, True, result_color)
             self.screen.blit(result_text_surface, (20, 60))
 
@@ -174,12 +155,11 @@ class FusionalVergenceGame:
 
     def run(self):
         pygame.display.set_caption(Strings.FUSIONAL_VERGENCE_TITLE)
-        pygame.mouse.set_visible(False)
+        pygame.mouse.set_visible(False)  # Hide the mouse cursor
 
         running = True
 
         while running:
-            self._update_dynamic_positions()
             running = self._handle_input()
             if running:
                 self._draw_scene()
